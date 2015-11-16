7f4a5b219d95b1247e156aa15086589135f69a88
Ionut <barsanionut62@yahoo.com>
2015-11-16 21:58:47 +0200

    First commit

---
+++ eSock-WS/src/main/java/Main.java
@@ -0,0 +1,10 @@
+import eSock_WS.persistance.entities.User;
+
+public class Main {
+
+	public static void main(String[] args) {
+		User user = new User();
+		user.test();
+	}
+
+}
---
+++ eSock-WS/src/main/java/eSock_WS/persistance/entities/User.java
@@ -0,0 +1,13 @@
+package eSock_WS.persistance.entities;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class User {
+	private static Logger LOGGER = LoggerFactory.getLogger(User.class);
+	
+	public void test() {
+		LOGGER.info("bai, merge");
+	}
+
+}
