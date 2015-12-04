29bd3524200184d0a7227636520c4eef38b0e508
Ionut <barsanionut62@yahoo.com>
2015-12-04 08:57:10 +0200

    Added db schema creation script

---
+++ eSock-WS/db-dev-schema.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE `esock_dev` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+CREATE TABLE `address` (
+  `address_id` int(11) NOT NULL,
+  `user_profile_id` int(11) NOT NULL,
+  `address_name` varchar(45) DEFAULT NULL,
+  `city` varchar(45) NOT NULL,
+  `postcode` varchar(45) NOT NULL,
+  `address_line_1` varchar(250) NOT NULL,
+  `address_line_2` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`address_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address_id` int(11) NOT NULL,
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `purchase` (
+  `purchase_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  PRIMARY KEY (`purchase_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `price` float NOT NULL,
+  `stock` int(11) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `user_profile_id` int(11) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user_profile` (
+  `user_profile_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `surname` varchar(45) NOT NULL,
+  `phone_number` varchar(45) DEFAULT NULL,
+  `email_address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`user_profile_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
