fc2d310695179f341964cf0bc204236eed60e0d6
IonutMarius <barsanionut62@yahoo.com>
2015-12-04 16:55:32 +0200

    Changed packages; Started adding configuration

---
+++ eSock-WS/src/main/java/eSock_WS/web/conf/WebContextConfig.java
@@ -0,0 +1,10 @@
+package eSock_WS.web.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+
+@Configuration
+@EnableWs
+public class WebContextConfig {
+
+}
---
+++ eSock-WS/src/main/java/eSock_WS/web/init/SpringWebAppInitializer.java
@@ -0,0 +1,26 @@
+package eSock_WS.web.init;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+
+import eSock_WS.web.conf.WebContextConfig;
+ 
+public class SpringWebAppInitializer implements WebApplicationInitializer {
+ 
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.register(WebContextConfig.class);
+         
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet(
+                "SpringMessageDispatcherServlet", new MessageDispatcherServlet(appContext));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+         
+    }
+}
