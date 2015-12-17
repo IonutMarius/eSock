f9a6826a60571a68ba2c8b377c0c30836dadd542
IonutMarius <barsanionut62@yahoo.com>
2015-12-17 18:21:34 +0200

    Added UserServiceImplTest

--- eSock-WS/src/test/java/ro/esock/model/persistance/config/JpaHibernateTestConfig.java
+++ eSock-WS/src/test/java/ro/esock/model/persistance/config/JpaHibernateTestConfig.java
@@ -2,7 +2,6 @@ package ro.esock.model.persistance.config;
 
 import java.util.Properties;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +11,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -49,14 +46,6 @@ public class JpaHibernateTestConfig {
 		dataSource.setUsername(env.getProperty("db.username"));
 		dataSource.setPassword(env.getProperty("db.password"));
 		return dataSource;
-	}
-
-	@Bean
-	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
-		JpaTransactionManager transactionManager = new JpaTransactionManager();
-		transactionManager.setEntityManagerFactory(emf);
-
-		return transactionManager;
 	}
 
 	Properties additionalProperties() {
---
+++ eSock-WS/src/test/java/ro/esock/model/persistance/service/UserServiceImplTest.java
@@ -0,0 +1,50 @@
+package ro.esock.model.persistance.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import ro.esock.model.persistance.TestUtils;
+import ro.esock.model.persistance.config.JpaHibernateTestConfig;
+import ro.esock.model.persistance.entitiy.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {JpaHibernateTestConfig.class})
+@Transactional
+public class UserServiceImplTest {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Test
+	public void saveAndFindUserTest(){
+		User expectedUser = TestUtils.createUser("1");
+		userService.create(expectedUser);	
+		User actualUser = userService.findById(expectedUser.getUserId());
+		
+		Assert.assertEquals(expectedUser, actualUser);
+	}
+	
+	@Test
+	public void saveAndDeleteUserTest(){
+		User user = userService.create(TestUtils.createUser("1"));
+		userService.remove(user);
+		user = userService.findById(user.getUserId());
+		
+		Assert.assertEquals(null, user);
+	}
+	
+	@Test
+	public void updateAndFindUserTest(){
+		User expectedUser = userService.create(TestUtils.createUser("1"));
+		expectedUser.setUsername("u0");
+		userService.update(expectedUser);
+		User actualUser = userService.findById(expectedUser.getUserId());
+
+		Assert.assertEquals(expectedUser, actualUser);
+	}
+}
