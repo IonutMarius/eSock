2dea0c3bd636804edc08a231ea3eba221298f443
IonutMarius <barsanionut62@yahoo.com>
2016-01-07 15:06:39 +0200

    Fixed - unit tests use test database

--- eSock-WS/src/test/java/ro/esock/model/persistance/config/JpaHibernateTestConfig.java
+++ eSock-WS/src/test/java/ro/esock/model/persistance/config/JpaHibernateTestConfig.java
@@ -2,6 +2,7 @@ package ro.esock.model.persistance.config;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,14 +12,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan("ro.esock.model")
+@ComponentScan({"ro.esock.model.converter", "ro.esock.model.repository", "ro.esock.model.service"})
 @PropertySource("classpath:test.properties")
 public class JpaHibernateTestConfig {
 	
@@ -46,6 +49,14 @@ public class JpaHibernateTestConfig {
 		dataSource.setUsername(env.getProperty("db.username"));
 		dataSource.setPassword(env.getProperty("db.password"));
 		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory emf){
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(emf);
+
+		return transactionManager;
 	}
 
 	Properties additionalProperties() {
